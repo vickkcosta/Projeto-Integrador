4525f980b33eb5b26752271df4aeab91acc30b97
Vitória Costa <160612006+vickkcosta@users.noreply.github.com>
2024-03-20 09:53:00 -0300

    Add files via upload

---
+++ trabalho1.py
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+c = 3
+d = 4
+
+frase = ("estou com fome")
+quantidade_espaços = frase.count(" ")
+print ("a quantidade de espaços na frase é", quantidade_espaços)
+palvra = ("vitória")
+quantidade_letras = len("vitória")
+print ("a quantidade de letras é", quantidade_letras)
+média = (a + b + c + d) / 4
+print ("a média é", média)
+quadrado = d * d
+print ("o quadrado é", quadrado)
+dobro = d * 2
+print ("o dobro é", dobro)
+if a > b:
+ print ("a é maior que b.")
+else:
+ print ("b é maior que a.")
+maior_numero = max (a,b,c,d)
+print ("o maior número é:", maior_numero)
